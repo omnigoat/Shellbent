408dce0c9a388a77070274e34c97f5caca38191f
omnigoat <omnigoat@gmail.com>
2020-04-19 18:34:11 +1000

    mad messing with colours for niceness

--- Shellbent/Models/WindowWrapper.cs
+++ Shellbent/Models/WindowWrapper.cs
@@ -70,8 +70,8 @@ namespace Shellbent.Models
 	internal struct TitleBarInfoBlockData
 	{
 		public string Text;
-		public Brush TextBrush;
-		public Brush BackgroundBrush;
+		public SolidColorBrush TextBrush;
+		public SolidColorBrush BackgroundBrush;
 	}
 	
 	internal struct TitleBarData
@@ -233,9 +233,9 @@ namespace Shellbent.Models
 		}
 	}
 
-	internal class WindowWrapper2017ToolWindowExpanded : WindowWrapper2017ToolWindow
+	internal class WindowWrapper2017ToolWindowExpanded : WindowWrapper2017
 	{
-		public new static bool IsSuitable(Window w)
+		public static bool IsSuitable(Window w)
 		{
 			if (WindowWrapper2017MainWindow.IsSuitable(w))
 				return false;
@@ -250,6 +250,13 @@ namespace Shellbent.Models
 		public WindowWrapper2017ToolWindowExpanded(Window window)
 			: base(window)
 		{ }
+
+		protected override UIElement RetrieveTitleBar()
+		{
+			return Window
+				.GetElement<UIElement>("TitleBar")
+				?.GetElement<Grid>();
+		}
 
 		protected override TextBlock RetrieveTitleBarTextBlock()
 		{
@@ -316,8 +323,8 @@ namespace Shellbent.Models
 		{
 			if (Window.IsActive)
 			{
-				var brightBrush = WindowUtils.CalculateBrightColorBrush(color);
-				var brighterBrush = WindowUtils.CalculateBrightColorBrush(brightBrush.Color);
+				var brightBrush = WindowUtils.CalculateHighlightBrush(color, 0.5f);
+				var brighterBrush = WindowUtils.CalculateHighlightBrush(brightBrush.Color, 0.5f);
 
 				ToolWindowBorder?.SetValue(Border.BackgroundProperty, brightBrush);
 				ToolWindowBorder?.SetValue(Border.BorderBrushProperty, brightBrush);
@@ -326,7 +333,7 @@ namespace Shellbent.Models
 			else
 			{
 				var brush = new SolidColorBrush(color);
-				var brightBrush = WindowUtils.CalculateBrightColorBrush(color);
+				var brightBrush = WindowUtils.CalculateHighlightBrush(color, 0.5f);
 
 				ToolWindowBorder?.SetValue(Border.BackgroundProperty, brush);
 				ToolWindowBorder?.SetValue(Border.BorderBrushProperty, brush);
@@ -357,7 +364,7 @@ namespace Shellbent.Models
 		// drag-handle brush (the ::::::::: thing to the side of the tool-window title)
 		private DrawingBrush cachedDragHandleBrush;
 		private DrawingBrush DragHandleBrush => cachedDragHandleBrush ??
-			(cachedDragHandleBrush = (DragHandle.Fill as DrawingBrush));
+			(cachedDragHandleBrush = (DragHandle?.Fill as DrawingBrush));
 
 		private Color? titleColor;
 	}
@@ -373,7 +380,52 @@ namespace Shellbent.Models
 
 		public WindowWrapper2019MainWindow(Window window)
 			: base(window)
-		{ }
+		{
+			Window.Deactivated += Window_ActivationChanged;
+			Window.Activated += Window_ActivationChanged;
+		}
+
+		private void Window_ActivationChanged(object sender, EventArgs e)
+		{
+			if (Window.IsActive)
+			{
+				foreach (var block in synthesizedInfoBlocks)
+				{
+					var textblock = block.Element?.GetElement<TextBlock>();
+					if (textblock == null)
+						continue;
+
+					if (!block.TextColor.HasValue)
+					{
+						textblock.ClearValue(TextBlock.ForegroundProperty);
+					}
+					else
+					{
+						textblock.SetValue(TextBlock.ForegroundProperty, new SolidColorBrush(block.TextColor.Value));
+					}
+				}
+			}
+			else
+			{
+				foreach (var block in synthesizedInfoBlocks)
+				{
+					var textblock = block.Element?.GetElement<TextBlock>();
+					if (textblock == null)
+						continue;
+
+					if (!block.TextColor.HasValue)
+					{
+						textblock.ClearValue(TextBlock.ForegroundProperty);
+					}
+					else
+					{
+						var darkBrush = WindowUtils.CalculateRelativeColorBrush(block.TextColor.Value, 0.75f);
+						textblock.SetValue(TextBlock.ForegroundProperty, darkBrush);
+					}
+
+				}
+			}
+		}
 
 		private class TitleInfoBlock
 		{
@@ -403,46 +455,57 @@ namespace Shellbent.Models
 			TitleBarVsMenu?.SetValue(Panel.BackgroundProperty, data.TitleBarBackgroundBrush);
 
 			// info-blocks
-			if (TitleBarInfoGrid != null)
+			if (TitleBarInfoGrid == null)
+				return;
+
+			
+			// if we have an override background-colour, then calculate a nice default
+			// background-colour for the blocks, and apply it to the prime block
+			if (data.TitleBarBackgroundBrush != null)
 			{
-				// if we have an override background-colour, then calculate a nice default
-				// background-colour for the blocks, and apply it to the prime block
-				if (data.TitleBarBackgroundBrush != null)
-				{
-					// just 25% less bright
-					PrimeTitleInfoBlock.Border.Background = new SolidColorBrush(data.TitleBarBackgroundBrush.Color * 0.75f);
-				}
-				else
-				{
-					PrimeTitleInfoBlock.Border.ClearValue(Border.BackgroundProperty);
-				}
+				// just 25% less bright
+				PrimeTitleInfoBlock.Border.Background = new SolidColorBrush(data.TitleBarBackgroundBrush.Color * 0.75f);
+			}
+			else
+			{
+				PrimeTitleInfoBlock.Border.ClearValue(Border.BackgroundProperty);
+			}
 
-				// remove all previously-synthesized info-blocks
-				synthesizedInfoBlocks.ForEach(TitleBarInfoGrid.Children.Remove);
+			// remove all previously-synthesized info-blocks
+			foreach (var block in synthesizedInfoBlocks.Select(x => x.Element))
+				TitleBarInfoGrid.Children.Remove(block);
 
-				// reset column-definitions from before
-				if (TitleBarInfoGrid.ColumnDefinitions.Count > 2)
-					TitleBarInfoGrid.ColumnDefinitions.RemoveRange(2, TitleBarInfoGrid.ColumnDefinitions.Count - 2);
+			// reset column-definitions from before
+			if (TitleBarInfoGrid.ColumnDefinitions.Count > 2)
+				TitleBarInfoGrid.ColumnDefinitions.RemoveRange(2, TitleBarInfoGrid.ColumnDefinitions.Count - 2);
 
-				if (data.Infos != null)
-				{
-					// add new column-definitions to match
-					foreach (var i in data.Infos)
-						TitleBarInfoGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+			if (data.Infos != null)
+			{
+				// add new column-definitions to match
+				foreach (var i in data.Infos)
+					TitleBarInfoGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
 
-					// recalculate title-bar-infos
-					synthesizedInfoBlocks = data.Infos
-						.Select(MakeInfoBlock)
-						.ToList();
+				// recalculate title-bar-infos
+				synthesizedInfoBlocks = data.Infos
+					.Select((x, idx) => new InfoBlock { Element = MakeInfoBlock(x, idx), TextColor = x.TextBrush?.Color })
+					.ToList();
 
-					// add all user-defined blocks
-					foreach (var c in synthesizedInfoBlocks)
-						TitleBarInfoGrid.Children.Add(c);
-				}
+				// reset all colours to what we are at the time
+				Window_ActivationChanged(null, null);
+
+				// add all user-defined blocks
+				foreach (var block in synthesizedInfoBlocks.Select(x => x.Element))
+					TitleBarInfoGrid.Children.Add(block);
 			}
 		}
 
-		protected List<Border> synthesizedInfoBlocks = new List<Border>();
+		protected struct InfoBlock
+		{
+			public Border Element;
+			public Color? TextColor;
+		}
+
+		protected List<InfoBlock> synthesizedInfoBlocks = new List<InfoBlock>();
 
 		protected override TextBlock RetrieveTitleBarTextBlock()
 		{
@@ -525,6 +588,12 @@ namespace Shellbent.Models
 					});
 				}
 
+				((r.Child as Border).Child as TextBlock).SetBinding(TextBlock.ForegroundProperty, new Binding()
+				{
+					Source = text,
+					Path = new PropertyPath("Foreground")
+				});
+
 				r.SetValue(Grid.ColumnProperty, idx + 2);
 				return r;
 			}
@@ -539,127 +608,30 @@ namespace Shellbent.Models
 		
 	}
 
-	internal class WindowWrapper2019ToolWindowExpanded : WindowWrapper2017ToolWindow
+
+	// these two actually had no differences
+	internal class WindowWrapper2019ToolWindowExpanded : WindowWrapper2017ToolWindowExpanded
 	{
 		public new static bool IsSuitable(Window w)
 		{
-			if (WindowWrapper2019MainWindow.IsSuitable(w))
-				return false;
-
-			var title_bar = w
-				?.GetElement<UIElement>("TitleBarContainer")
-				?.GetElement<UIElement>("TitleBar");
-
-			return title_bar != null && VisualTreeHelper.GetChildrenCount(title_bar) > 0;
+			return WindowWrapper2017ToolWindowExpanded.IsSuitable(w);
 		}
 
 		public WindowWrapper2019ToolWindowExpanded(Window window)
 			: base(window)
 		{ }
-
-		protected override TextBlock RetrieveTitleBarTextBlock()
-		{
-			return TitleBar?.GetElement<TextBlock>("WindowTitle");
-		}
 	}
 
 	internal class WindowWrapper2019ToolWindow : WindowWrapper2017ToolWindow
 	{
 		public new static bool IsSuitable(Window w)
 		{
-			if (WindowWrapper2019MainWindow.IsSuitable(w))
-				return false;
-
-			var title_bar = w
-				?.GetElement<UIElement>("TitleBarContainer")
-				?.GetElement<UIElement>("TitleBar");
-
-			return title_bar != null && VisualTreeHelper.GetChildrenCount(title_bar) == 0;
+			return WindowWrapper2017ToolWindow.IsSuitable(w);
 		}
 
 		public WindowWrapper2019ToolWindow(Window window)
 			: base(window)
-		{
-			Window.Activated += Window_ActivationChanged;
-			Window.Deactivated += Window_ActivationChanged;
-		}
-
-		private void Window_ActivationChanged(object sender, EventArgs e)
-		{
-			if (!titleColor.HasValue)
-				return;
-
-			UpdateToolWindowColors(titleColor.Value);
-		}
-
-		protected override TextBlock RetrieveTitleBarTextBlock()
-		{
-			return null;
-		}
-
-		public override void UpdateTitleBar(TitleBarData data)
-		{
-			titleColor = data.TitleBarBackgroundBrush?.Color;
-
-			if (data.TitleBarBackgroundBrush != null)
-			{
-				UpdateToolWindowColors(data.TitleBarBackgroundBrush.Color);
-			}
-			else
-			{
-				ToolWindowBorder?.ClearValue(Border.BackgroundProperty);
-				DragHandle.ClearValue(Shape.FillProperty);
-			}
-		}
-
-		private void UpdateToolWindowColors(Color color)
-		{
-			if (Window.IsActive)
-			{
-				var brightBrush = WindowUtils.CalculateBrightColorBrush(color);
-				var brighterBrush = WindowUtils.CalculateBrightColorBrush(brightBrush.Color);
-
-				ToolWindowBorder?.SetValue(Border.BackgroundProperty, brightBrush);
-				ToolWindowBorder?.SetValue(Border.BorderBrushProperty, brightBrush);
-				DragHandle?.SetValue(Shape.FillProperty, GenerateFillBrush(brighterBrush));
-			}
-			else
-			{
-				var brush = new SolidColorBrush(color);
-				var brightBrush  = WindowUtils.CalculateBrightColorBrush(color);
-
-				ToolWindowBorder?.SetValue(Border.BackgroundProperty, brush);
-				ToolWindowBorder?.SetValue(Border.BorderBrushProperty, brush);
-				DragHandle.SetValue(Shape.FillProperty, GenerateFillBrush(brightBrush));
-			}
-		}
-
-		private Brush GenerateFillBrush(Brush brush)
-		{
-			var vsgeom = DragHandleBrush.Drawing as GeometryDrawing;
-			var geom = new GeometryDrawing(brush, vsgeom.Pen, vsgeom.Geometry);
-			var fillBrush = DragHandleBrush.Clone();
-			fillBrush.Drawing = geom;
-			return fillBrush;
-		}
-
-		private Border cachedToolWindowBorder;
-		private Border ToolWindowBorder => cachedToolWindowBorder ??
-			(cachedToolWindowBorder = Window
-				?.GetElement<Border>("ContentBorder")
-				?.GetElement<Border>("Bd"));
-
-		private Rectangle cachedDragHandle;
-		private Rectangle DragHandle => cachedDragHandle ??
-			(cachedDragHandle = ToolWindowBorder
-				?.GetElement<Rectangle>("DragHandleTexture"));
-
-		// drag-handle brush (the ::::::::: thing to the side of the tool-window title)
-		private DrawingBrush cachedDragHandleBrush;
-		private DrawingBrush DragHandleBrush => cachedDragHandleBrush ??
-			(cachedDragHandleBrush = (DragHandle.Fill as DrawingBrush));
-
-		private Color? titleColor;
+		{ }
 	}
 
 
@@ -701,7 +673,7 @@ namespace Shellbent.Models
 				return null;
 		}
 
-		public static SolidColorBrush CalculateBrightColorBrush(Color? color)
+		public static SolidColorBrush CalculateRelativeColorBrush(Color? color, float mulitplier)
 		{
 			if (!color.HasValue)
 				return null;
@@ -709,7 +681,25 @@ namespace Shellbent.Models
 			// perceived luminance
 			float luminance = (0.299f * color.Value.R + 0.587f * color.Value.G + 0.114f * color.Value.B) / 255.0f;
 
-			return new SolidColorBrush(color.Value * (1.4f / (float)Math.Sqrt(luminance)));
+			return new SolidColorBrush(color.Value * (1.0f + (mulitplier - 1.0f) * (float)Math.Sqrt(luminance)));
+		}
+
+		public static SolidColorBrush CalculateHighlightBrush(Color? color, float delta)
+		{
+			if (!color.HasValue)
+				return null;
+
+			float sum = color.Value.R + color.Value.G + color.Value.B;
+			byte dR = (byte)((color.Value.R / sum) * (255.0f * delta));
+			byte dG = (byte)((color.Value.G / sum) * (255.0f * delta));
+			byte dB = (byte)((color.Value.B / sum) * (255.0f * delta));
+
+			var c = Color.FromRgb(
+				(byte)Math.Min(color.Value.R + dR, 255),
+				(byte)Math.Min(color.Value.G + dG, 255),
+				(byte)Math.Min(color.Value.B + dB, 255));
+
+			return new SolidColorBrush(c);
 		}
 	}
 }
